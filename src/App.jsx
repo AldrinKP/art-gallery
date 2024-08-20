6f1e95746827570316ab566aabef7b7132efe966
AldrinKP <aldrinpastores@gmail.com>
2024-08-20 14:07:18 +0200

    Added theme colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
 	return (
 		<>
-			<h1 className="font-heading text-heading-xl font-heading-xl">
+			<h1 className="font-heading text-size-heading-xl leading-line-heading-xl text-gold">
 				Gallery Website
 			</h1>
 			<p className="font-body text-l">Sample text to test font</p>
