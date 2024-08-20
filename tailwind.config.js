8f07a3ae2e0d028e639b8af7f76e0efb2ff2e593
AldrinKP <aldrinpastores@gmail.com>
2024-08-20 13:52:58 +0200

    Installed and configured fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				heading: ['Big Shoulders Display', 'sans-serif'],
+				body: ['Outfit', 'sans-serif'],
+			},
+			fontSize: {
+				'body-s': '18px',
+				'body-m': '22px',
+				'heading-s': '36px',
+				'heading-m': '60px',
+				'heading-l': '70px',
+				'heading-xl': '96px',
+			},
+			lineHeight: {
+				'body-s': '28px',
+				'body-m': '32px',
+				'heading-s': '36px',
+				'heading-m': '50px',
+				'heading-l': '70px',
+				'heading-xl': '88px',
+			},
+		},
 	},
 	plugins: [],
 };
