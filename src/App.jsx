3da9e6b2e82b49e8af18f99844f23dec85a9fd71
AldrinKP <aldrinpastores@gmail.com>
2024-08-22 15:49:31 +0200

    Wrapped all sections in a flex-col div to better align them

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import IntroSection from './components/home-page/IntroSection';
 
 function App() {
 	return (
-		<>
+		<div className="flex flex-col items-center">
 			<IntroSection />
 			<BentoSection />
 			<Footer />
-		</>
+		</div>
 	);
 }
 
