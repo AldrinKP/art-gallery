8e92527f1be0d0b05deb556d09f921a2feb77e55
AldrinKP <aldrinpastores@gmail.com>
2024-08-22 15:19:42 +0200

    Completed Bento Section

--- src/components/home-page/BentoSection.jsx
+++ src/components/home-page/BentoSection.jsx
@@ -6,7 +6,7 @@ const BentoSection = () => {
 	return (
 		<div className="flex h-full w-[1110px] mx-auto my-[180px] items-center justify-center">
 			<div className="grid h-full w-full gap-8 bg-transparent grid-cols-5 grid-rows-3">
-				<div className="col-span-2 row-span-1 flex flex-col justify-center mr-32 w-[350px]">
+				<div className="col-span-2 row-span-3 flex flex-col justify-center mr-32 w-[350px]">
 					<h1 className="font-heading font-black text-size-heading-m leading-line-heading-m text-almost-black mb-8">
 						YOUR DAY AT THE GALLERY
 					</h1>
@@ -16,31 +16,31 @@ const BentoSection = () => {
 						their creative process.
 					</p>
 				</div>
-				<div className="col-span-3 row-span-1">
+				<div className="col-span-3 row-span-3">
 					<img src={gridImage1} alt="art exhibit 1" />
 				</div>
-				<div className="col-span-3 row-span-2 flex justify-center">
+				<div className="col-span-3 row-span-5 flex justify-center">
 					<img
 						src={gridImage2}
 						alt="bench in front of an art piece"
 					/>
 				</div>
-				<div className="col-span-2 row-span-1 flex justify-center">
+				<div className="col-span-2 row-span-5 flex flex-col justify-center">
 					<img
 						src={gridImage3}
 						alt="People looking at art"
 						className="object-cover"
 					/>
-				</div>
-				<div className="col-span-2 row-span-1 flex items-center justify-center bg-almost-black">
-					<div className="flex flex-col w-[350px]">
-						<h1 className="font-heading font-black text-size-heading-m leading-line-heading-m text-white mb-8">
-							COME & BE INSPIRED
-						</h1>
-						<p className="font-body text-size-body-m leading-line-body-m text-white">
-							We&apos;re excited to welcome you to our gallery and
-							see how our collections influence you.
-						</p>
+					<div className="flex items-center justify-center flex-auto bg-almost-black mt-8">
+						<div className="flex flex-col w-[350px]">
+							<h1 className="font-heading font-black text-size-heading-m leading-line-heading-m text-white mb-8">
+								COME & BE INSPIRED
+							</h1>
+							<p className="font-body text-size-body-m leading-line-body-m text-white">
+								We&apos;re excited to welcome you to our gallery
+								and see how our collections influence you.
+							</p>
+						</div>
 					</div>
 				</div>
 			</div>
