c1d487fa2d9c1b6270f26d4c89eb8dc81348ac24
AldrinKP <aldrinpastores@gmail.com>
2024-08-24 10:37:26 +0200

    Added Route for Location page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Location from './pages/Location';
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<Home />} />
+				<Route path="/" element={<Home />}>
+					<Route path="location" element={Location} />
+				</Route>
 			</Routes>
 		</BrowserRouter>
 	);
