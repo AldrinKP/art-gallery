5e81ced1757dcf6b8ff2ea88b808ed49a915d46d
AldrinKP <aldrinpastores@gmail.com>
2024-08-24 12:03:22 +0200

    Integrated Jawg map tile

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,5 +1,22 @@
+import { MapContainer, Marker, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import { Icon } from 'leaflet';
+
 const Map = () => {
-	return <div id="map" className="w-full h-[600px]"></div>;
+	return (
+		<MapContainer
+			center={[41.48148034531764, -71.31036692421809]}
+			zoom={15}
+			scrollWheelZoom={false}
+			className="h-[600px]"
+		>
+			<TileLayer
+				attribution=""
+				url="https://tile.jawg.io/jawg-light/{z}/{x}/{y}{r}.png?access-token=MO8rZa0B1M14CmufZP1cjNqgNnO5yC1SatFp9PMrrBYc9cow1w3IYC10EL8TiWNh"
+			/>
+			<Marker position={[41.4815044584745, -71.3103669241421]}></Marker>
+		</MapContainer>
+	);
 };
 
 export default Map;
