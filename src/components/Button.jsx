badf0d420bde835fc837d4e881e38351d1924517
AldrinKP <aldrinpastores@gmail.com>
2024-08-24 10:58:24 +0200

    Updated Button to render a Link component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,9 @@
-const Button = ({ type, onClick }) => {
+import { Link } from 'react-router-dom';
+
+const Button = ({ type }) => {
 	if (type === 'location') {
 		return (
-			<button className="group flex w-[260px] h-[72px]" onClick={onClick}>
+			<Link className="group flex w-[260px] h-[72px]" to="/location">
 				<div className="h-full w-[204px] bg-almost-black group-hover:bg-gold flex justify-center items-center">
 					<span className="font-heading font-extrabold text-white tracking-button">
 						OUR LOCATION
@@ -16,11 +18,11 @@ const Button = ({ type, onClick }) => {
 						<path stroke="#FFF" fill="none" d="m1 0 8 12-8 12" />
 					</svg>
 				</div>
-			</button>
+			</Link>
 		);
 	}
 	return (
-		<button className="group flex w-[260px] h-[72px]" onClick={onClick}>
+		<Link className="group flex w-[260px] h-[72px]" to="/">
 			<div className="w-[56px] h-full bg-gold group-hover:bg-almost-black flex justify-center items-center">
 				<svg width="10" height="24" xmlns="http://www.w3.org/2000/svg">
 					<path stroke="#FFF" fill="none" d="M9 24 1 12 9 0" />
@@ -31,7 +33,7 @@ const Button = ({ type, onClick }) => {
 					BACK TO HOME
 				</span>
 			</div>
-		</button>
+		</Link>
 	);
 };
 
