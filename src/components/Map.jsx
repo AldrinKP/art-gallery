d6079459db3f63635317053f3996cb509659b58d
AldrinKP <aldrinpastores@gmail.com>
2024-08-24 12:13:08 +0200

    Added custom marker Icon

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,8 +1,12 @@
 import { MapContainer, Marker, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import { Icon } from 'leaflet';
+import { DivIcon } from 'leaflet';
 
 const Map = () => {
+	const customIcon = new DivIcon({
+		html: '<svg width="66" height="88" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M0 56V0h66v56H45L33 88 21 56H0Z" fill="#151515"/><circle fill="#D5966C" cx="33" cy="28" r="12"/></g></svg>',
+	});
+
 	return (
 		<MapContainer
 			center={[41.48148034531764, -71.31036692421809]}
@@ -14,7 +18,10 @@ const Map = () => {
 				attribution=""
 				url="https://tile.jawg.io/jawg-light/{z}/{x}/{y}{r}.png?access-token=MO8rZa0B1M14CmufZP1cjNqgNnO5yC1SatFp9PMrrBYc9cow1w3IYC10EL8TiWNh"
 			/>
-			<Marker position={[41.4815044584745, -71.3103669241421]}></Marker>
+			<Marker
+				position={[41.4815044584745, -71.3103669241421]}
+				icon={customIcon}
+			></Marker>
 		</MapContainer>
 	);
 };
