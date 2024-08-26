92e8251385e3d06f5c71109c5589c33d6e2ae694
AldrinKP <aldrinpastores@gmail.com>
2024-08-26 11:11:17 +0200

    Resized "Your day" text

--- src/components/home-page/BentoSection.jsx
+++ src/components/home-page/BentoSection.jsx
@@ -4,13 +4,13 @@ import gridImage3 from '../../assets/desktop/image-grid-3@2x.jpg';
 
 const BentoSection = () => {
 	return (
-		<section className="flex h-full max-w-[1110px] mx-auto my-[180px] items-center justify-center">
+		<section className="flex h-full max-w-[1110px] mx-auto my-[180px] items-center justify-center lg:p-0 px-8">
 			<div className="grid h-full w-full gap-8 bg-transparent grid-cols-5 grid-rows-3">
-				<div className="col-span-2 row-span-3 flex flex-col justify-center mr-32 w-[350px]">
-					<h1 className="font-heading font-black text-size-heading-m leading-line-heading-m text-almost-black mb-8">
+				<div className="col-span-2 row-span-3 flex flex-col justify-center lg:mr-32 mr-16 lg:w-[350px] min-w-[225px]">
+					<h1 className="font-heading font-black lg:text-size-heading-m text-[50px] lg:leading-line-heading-m leading-[45px] text-almost-black mb-8">
 						YOUR DAY AT THE GALLERY
 					</h1>
-					<p className="font-body text-size-body-m leading-line-body-m text-dark-grey">
+					<p className="font-body lg:text-size-body-m text-size-body-s lg:leading-line-body-m leading-line-body-s text-dark-grey">
 						Wander through our distinct collections and find new
 						insights about our artists. Dive into the details of
 						their creative process.
