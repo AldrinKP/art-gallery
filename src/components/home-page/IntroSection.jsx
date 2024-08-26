b8e6727dfbe6e5e303c7e8ffc2e263c5edec8075
AldrinKP <aldrinpastores@gmail.com>
2024-08-26 11:00:47 +0200

    Resized text

--- src/components/home-page/IntroSection.jsx
+++ src/components/home-page/IntroSection.jsx
@@ -11,13 +11,13 @@ const IntroSection = () => {
 				<img src={heroImage} className="w-full h-full object-cover" />
 			</div>
 
-			<div className="flex lg:flex-row flex-col lg:gap-80 absolute lg:left-[165px] left-[389px] lg:top-[189px] top-[145px] w-full">
-				<h1 className="font-heading font-black text-size-heading-xl leading-line-heading-xl text-black lg:text-white lg:mix-blend-difference">
+			<div className="flex lg:flex-row flex-col lg:gap-80 gap-12 absolute lg:left-[165px] left-[389px] lg:top-[189px] top-[145px] w-full">
+				<h1 className="font-heading font-black lg:text-size-heading-xl text-size-heading-l lg:leading-line-heading-xl leading-line-heading-l text-black lg:text-white lg:mix-blend-difference">
 					MODERN <br />
 					ART GALLERY
 				</h1>
-				<div className="flex flex-col gap-[64px]">
-					<p className="w-[350px] font-body text-size-body-m leading-line-body-m text-dark-grey">
+				<div className="flex flex-col lg:gap-16 gap-12">
+					<p className="w-[350px] font-body lg:text-size-body-m text-size-body-s lg:leading-line-body-m leading-line-body-s text-dark-grey">
 						The arts in the collection of the Modern Art Gallery all
 						started from a spark of inspiration. Will these pieces
 						inspire you? Visit us and find out.
