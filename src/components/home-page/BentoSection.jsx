b8cb60bb98912397ad1f3de20effcc43959de516
AldrinKP <aldrinpastores@gmail.com>
2024-08-27 11:07:34 +0200

    Added flex layout on mobile view and added ordering of divs

--- src/components/home-page/BentoSection.jsx
+++ src/components/home-page/BentoSection.jsx
@@ -11,18 +11,8 @@ import gridImage3mobile from '../../assets/mobile/image-grid-3@2x.jpg';
 const BentoSection = () => {
 	return (
 		<section className="flex h-full max-w-[1110px] mx-auto my-[180px] items-center justify-center lg:p-0 px-8">
-			<div className="grid h-full w-full lg:gap-8 gap-[11px] bg-transparent grid-cols-5 grid-rows-3">
-				<div className="col-span-2 row-span-3 flex flex-col justify-center lg:mr-32 mr-16 lg:w-[350px] min-w-[225px]">
-					<h1 className="font-heading font-black lg:text-size-heading-m text-[50px] lg:leading-line-heading-m leading-[45px] text-almost-black lg:mb-8 mb-6">
-						YOUR DAY AT THE GALLERY
-					</h1>
-					<p className="font-body lg:text-size-body-m text-size-body-s lg:leading-line-body-m leading-line-body-s text-dark-grey">
-						Wander through our distinct collections and find new
-						insights about our artists. Dive into the details of
-						their creative process.
-					</p>
-				</div>
-				<div className="col-span-3 row-span-3 min-h-[400px]">
+			<div className="md:grid flex flex-col h-full w-full lg:gap-8 gap-[11px] bg-transparent grid-cols-5 grid-rows-3">
+				<div className="col-span-3 row-span-3 md:order-2 min-h-[400px]">
 					<picture>
 						<source
 							media="(min-width: 1024px)"
@@ -39,7 +29,17 @@ const BentoSection = () => {
 						/>
 					</picture>
 				</div>
-				<div className="col-span-3 row-span-5 flex justify-center min-h-[720px]">
+				<div className="col-span-2 row-span-3 md:order-1 flex flex-col justify-center lg:mr-32 mr-16 lg:w-[350px] min-w-[225px]">
+					<h1 className="font-heading font-black lg:text-size-heading-m text-[50px] lg:leading-line-heading-m leading-[45px] text-almost-black lg:mb-8 mb-6">
+						YOUR DAY AT THE GALLERY
+					</h1>
+					<p className="font-body lg:text-size-body-m text-size-body-s lg:leading-line-body-m leading-line-body-s text-dark-grey">
+						Wander through our distinct collections and find new
+						insights about our artists. Dive into the details of
+						their creative process.
+					</p>
+				</div>
+				<div className="col-span-3 row-span-5 md:order-3 flex justify-center min-h-[720px]">
 					<picture>
 						<source
 							media="(min-width: 1024px)"
@@ -56,7 +56,7 @@ const BentoSection = () => {
 						/>
 					</picture>
 				</div>
-				<div className="col-span-2 row-span-5 flex flex-col justify-center min-h-[313px]">
+				<div className="col-span-2 row-span-5 md:order-4 flex flex-col justify-center min-h-[313px]">
 					<div className="basis-[40%]">
 						<picture>
 							<source
@@ -74,7 +74,6 @@ const BentoSection = () => {
 							/>
 						</picture>
 					</div>
-
 					<div className="flex items-center justify-center flex-auto bg-almost-black lg:mt-8 mt-[11px]">
 						<div className="flex flex-col lg:w-[350px] lg:px-0 px-6">
 							<h1 className="font-heading font-black lg:text-size-heading-m text-[50px] lg:leading-line-heading-m leading-[50px] text-white lg:mb-8 mb-6">
