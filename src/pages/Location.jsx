03ccdc3d261315ccb87f6f809e7a28842cd01bd0
AldrinKP <aldrinpastores@gmail.com>
2024-08-28 14:10:55 +0200

    Fixed width so line breaks after "OUR"

--- src/pages/Location.jsx
+++ src/pages/Location.jsx
@@ -9,10 +9,8 @@ const Location = () => {
 			<Button type="home" className="absolute top-0 left-32" />
 			<div className="flex place-content-center w-full md:py-24 py-14 bg-almost-black">
 				<div className="flex md:flex-row flex-col flex-grow justify-between lg:max-w-[1110px] lg:px-0 px-10">
-					<h1 className="font-heading font-black lg:text-size-heading-l text-[55px] lg:leading-line-heading-l leading-[50px] text-white md:mb-0 mb-12">
-						OUR
-						<br />
-						LOCATION
+					<h1 className="font-heading font-black lg:text-size-heading-l text-[55px] lg:leading-line-heading-l leading-[50px] text-white md:mb-0 mb-12 md:w-48">
+						OUR LOCATION
 					</h1>
 					<div className="flex flex-col">
 						<h2 className="font-heading lg:text-size-heading-s text-[32px] lg:leading-line-heading-s leading-[32px] text-gold mb-6">
